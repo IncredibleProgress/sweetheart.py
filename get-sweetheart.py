f6efcdca74eb353c0713500514d992a559ef4939
Nicolas Champion <champion.nicolas@gmail.com>
2022-01-24 12:08:37 +0100

    update installation process

--- get-sweetheart.py
+++ get-sweetheart.py
@@ -10,43 +10,48 @@ NOTE: this script has been tested on Ubuntu 20.04 only
 import os,sys,stat,json
 from subprocess import run
 
-__version__  = "0.1.2"
+__version__  = "0.1.3"
 
 # paths setting
-POETRY_BIN = f"{os.environ['HOME']}/.local/bin/poetry"
-CONFIG_PATH = f"{os.environ['HOME']}/.sweet/sweetheart/configuration"
-SWS_PATH = f"{os.environ['HOME']}/.sweet/sweetheart/programs/scripts"
+PATHS = {
+    'config': f"{os.environ['HOME']}/.sweet/sweetheart/configuration",
+    'scripts': f"{os.environ['HOME']}/.sweet/sweetheart/programs/scripts",
+    'python': f"{os.environ['HOME']}/.sweet/sweetheart/programs/my_python" }
+
+# make required directories
+for basedir in PATHS.items():
+    os.makedirs(basedir,exist_ok=True)
 
 # for getting bash standard output
 bash_stdout = lambda cmd:\
-    run(cmd,text=True,capture_output=True,shell=True).stdout.strip().lower()
+    run(cmd,text=True,capture_output=True,shell=True).stdout.strip()
 
 # get distrib infos on debian/ubuntu
-distrib = bash_stdout("lsb_release -is")
-codename = bash_stdout("lsb_release -cs")
+distrib = bash_stdout("lsb_release -is").lower()
+codename = bash_stdout("lsb_release -cs").lower()
+
+# poetry commands
+POETRY_BIN = f"{os.environ['HOME']}/.local/bin/poetry"
+POETRY_INSTALL = "curl -sSL https://install.python-poetry.org | python3 -"
+POETRY_INIT = f"{os.environ['HOME']}/.local/bin/poetry init -n"
+POETRY_ADD = f"{os.environ['HOME']}/.local/bin/poetry add sweetheart"
 
 # python-poetry is required
 if not os.path.isfile(POETRY_BIN):
-    run("curl -sSL https://install.python-poetry.org | python3 -",shell=True)
+    run(POETRY_INSTALL,shell=True)
 
 # create python directories
-os.makedirs(SWS_PATH,exist_ok=True)
-os.chdir(f"{SWS_PATH}/..")
-run([POETRY_BIN,"new","my_python"])
-
-# install python dependencies
-os.chdir("./my_python")
-run([POETRY_BIN,"add","sweetheart"])
+os.makedirs(PATHS["python"],exist_ok=True)
+os.chdir(PATHS["python"])
+run(POETRY_INIT,shell=True)
+run(POETRY_ADD,shell=True)
 
 # set python env
-venv = run([POETRY_BIN,"env","info","--path"],
-    text=True,capture_output=True).stdout.strip()
-if venv == "":
-    raise Exception("Error, no python env found")
+venv = bash_stdout(f"{POETRY_BIN} env info --path")
+if venv == "": raise Exception("Error, no python env found")
 
 # set subroc.conf
-os.makedirs(CONFIG_PATH,exist_ok=True)
-with open(f"{CONFIG_PATH}/subproc.json","w") as file_out:
+with open(f"{PATHS['config']}/subproc.json","w") as file_out:
     json.dump({'pyenv':venv},file_out)
 
 # set RethinkDB repository
@@ -61,7 +66,7 @@ sudo apt-get update
     """.splitlines(): run(instruc.strip(),shell=True)
 
 # set SWeetheart Shell command -> sws
-with open(f"{SWS_PATH}/sws","w") as file_out:
+with open(f"{PATHS['scripts']}/sws","w") as file_out:
     file_out.write(f"""
 
 #!/bin/sh
@@ -70,20 +75,20 @@ with open(f"{SWS_PATH}/sws","w") as file_out:
 
     """.strip())
 
-os.chmod(f"{SWS_PATH}/sws",stat.S_IRWXU|stat.S_IRGRP|stat.S_IROTH)
+os.chmod(f"{PATHS['scripts']}/sws",stat.S_IRWXU|stat.S_IRGRP|stat.S_IROTH)
 print(f"\n[SWEETHEART] Welcome {os.environ['USER'].capitalize()} !",
     f"\nthe 'sws' command is now available for {distrib} {codename}")
 
 if '--local-bin' in sys.argv:
-    run(f"sudo ln -s {SWS_PATH}/sws /usr/local/bin/",shell=True)
+    run(f"sudo ln -s {PATHS['scripts']}/sws /usr/local/bin/",shell=True)
 
-elif not SWS_PATH in os.environ['PATH']:
+elif not PATHS['scripts'] in os.environ['PATH']:
     with open(f"{os.environ['HOME']}/.bashrc","a") as fi:
-        fi.write(f"\nexport PATH={SWS_PATH}:$PATH")
-        print(f"{SWS_PATH} added to $PATH within ~./bashrc")
+        fi.write(f"\nexport PATH={PATHS['scripts']}:$PATH")
+        print(f"{PATHS['scripts']} added to $PATH within ~./bashrc")
 
 # exit message
 print("\n[SWEETHEART] all done setting Sweetheart requirements",
     "\nif needed set 'config.json' and 'subproc.json' within configuration directory",
-    "\nthen restart bash and type 'sws sweet --init' for installing provided components",
+    "\nthen restart bash and type 'sws --init' for installing usual components",
     end="\n\n")
